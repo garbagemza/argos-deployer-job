29193d5878b573a26e7166f11e3369c36d941209
garbagemza <garbagemza@gmail.com>
2022-05-22 20:20:55 -0300

    fixed failure mapping function

--- controllers/postTicketController.js
+++ controllers/postTicketController.js
@@ -1,7 +1,7 @@
 const { dbService } = require('../services')
 
 module.exports = function(req, res) {
-    const sum = dbService.yourFunctions(1, 2)
+    const sum = dbService.yourFunction(1, 2)
 
     res.send('The sum is: ' + sum)
 }
