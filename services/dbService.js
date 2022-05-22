c9a5e1948c9765d8ca7de20cf029373df10e2d9e
garbagemza <garbagemza@gmail.com>
2022-05-22 20:49:09 -0300

    creating first attempts to database

--- services/dbService.js
+++ services/dbService.js
@@ -1,5 +1,10 @@
+const { logger } = require('../utilities')
+const db = require('better-sqlite3')('foobar.db', { verbose: logger.info });
+
 function yourFunction(a, b) {
-    return a + b
+    const stmt = db.prepare('CREATE TABLE IF NOT EXISTS cats (name STRING, age INTEGER);')
+    const result = stmt.run()
+    return result
 }
 
 module.exports = {
