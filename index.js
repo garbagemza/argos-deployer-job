708305e97259724d508ee4b63f66c61f7f7a18a5
garbagemza <garbagemza@gmail.com>
2022-05-22 11:17:46 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express')
+const createError = require('http-errors')
+
+const { envcheck } = require('./utilities')
+
+const { catchAll, health } = require('./middlewares')
+
+const { 
+	getYourPathController
+} = require('./controllers')
+
+const app = express()
+
+// this checks the existence of environment variables, if they are not declared
+// an exception will be thrown.
+envcheck(['PORT'])
+
+// add your middleware here
+app.get('/your/path', getYourPathController)
+
+// catch for liveness probe
+health(app)
+
+// this middleware catches the unhandled paths
+catchAll(app)
+
+app.listen(process.env.PORT)
+
