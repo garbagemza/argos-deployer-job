f290ba40d6c4d71d9b80ca9fce24c7f6216fa283
garbagemza <garbagemza@gmail.com>
2022-05-25 22:00:03 -0300

    making use of user version for database so we can trigger db creation or updates

--- utilities/db-migrator.js
+++ utilities/db-migrator.js
@@ -1,9 +1,17 @@
 const logger = require('./logger.js')
-const db = require('better-sqlite3')(process.env['WORKDIR'] + 'deploys.db', { verbose: logger.info });
+
+const db = require('better-sqlite3')(process.env['WORKDIR'] + '/deploys.db', { verbose: logger.info })
 
 const dbMigrator = function() {
     const stmt = db.prepare('CREATE TABLE IF NOT EXISTS deploys (id INTEGER PRIMARY KEY, uuid STRING NOT NULL, issuedAt STRING NOT NULL);')
     const result = stmt.run()
+    logger.info(`current db version: ${userVersion()}`)
+    
+}
+
+const userVersion = function() {
+    const row = db.prepare('PRAGMA user_version;').get()
+    return row.user_version
 }
 
 module.exports = dbMigrator
