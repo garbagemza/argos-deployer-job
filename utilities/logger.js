95618811adbe231472ec789dfbbf121731e499a2
garbagemza <garbagemza@gmail.com>
2022-05-30 09:48:49 -0300

    moving logs per #1 to dedicated folder

--- utilities/logger.js
+++ utilities/logger.js
@@ -1,32 +1,36 @@
 const winston = require('winston')
+require('winston-daily-rotate-file')
+
+const logFormat = winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    winston.format.align(),
+    winston.format.printf(
+        info => `${info.timestamp} ${info.level}: ${info.message}`,
+   ),
+)
+
+const dailyRotation = new winston.transports.DailyRotateFile({
+    filename: `${process.env['WORKDIR']}/logs/application-%DATE%.log`,
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: false,
+    maxFiles: '10d'
+})
+
+const consoleTransport = new winston.transports.Console({
+    level: 'debug',
+    timestamp: true,
+    format: winston.format.combine(
+        winston.format.cli()
+    )
+})
 
 const logger = winston.createLogger({
     level: process.env.LOG_LEVEL || 'info',
+    format: logFormat,
     transports: [
-      new winston.transports.File({
-        filename: 'combined.log',
-        timestamp: true,
-        format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.json(), 
-          )
-      }),
-      new winston.transports.File({
-        filename: 'app-error.log',
-        level: 'error',
-        timestamp: true,
-        format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.json(), 
-          )
-      }),
-      new winston.transports.Console({
-          level: 'debug',
-          timestamp: true,
-          format: winston.format.combine(
-            winston.format.cli()
-          )
-      })
+        dailyRotation,
+        consoleTransport
     ],
 })
 
