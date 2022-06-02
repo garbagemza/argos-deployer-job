228608c5af8b9e2079759d2387ea14c41c9b03be
garbagemza <garbagemza@gmail.com>
2022-06-02 08:46:21 -0300

    #2 handled pending tickets by returning existing entry on db

--- services/dbService.js
+++ services/dbService.js
@@ -2,21 +2,22 @@ const { database } = require('../utilities')
 const { nanoid } = require('nanoid')
 
 function postTicket(userName, repositoryName) {
-    const lastPendingTicket = getLastPendingTicket(userName, repositoryName)
-    if (lastPendingTicket != null) {
-        return lastPendingTicket.uuid
+    const pendingTicket = getPendingTicket(userName, repositoryName)
+    if (pendingTicket != null) {
+        return pendingTicket.uuid
     } else {
         return saveTicket(userName, repositoryName)
     }
 }
 
 function saveTicket(userName, repositoryName) {
+    const initialStatus = 'PENDING'
     const uuid = nanoid()
-    const insertDeploy = `INSERT INTO deploys (uuid, engine, user, repo) VALUES ('${uuid}', 'github', '${userName}', '${repositoryName}');`
+    const insertDeploy = `INSERT INTO deploys (uuid, status, engine, user, repo) VALUES ('${uuid}', '${initialStatus}', 'github', '${userName}', '${repositoryName}');`
     const insertStatus = `INSERT INTO deploy_statuses
                                 (deployUuid, status, reason, date)
                             VALUES
-                                ('${uuid}', 'PENDING', 'User triggered deploy for ${userName}/${repositoryName}', datetime());`
+                                ('${uuid}', '${initialStatus}', 'User triggered deploy for ${userName}/${repositoryName}', datetime());`
 
     const statements = [insertDeploy, insertStatus]
     database.runInTransaction(statements)
@@ -24,20 +25,18 @@ function saveTicket(userName, repositoryName) {
 }
 
 /**
- * Gets the last pending ticket
+ * Get pending ticket based on userName and repositoryName
  *
  * @param {string} userName
  * @param {string} repositoryName
- * @returns last pending ticket or null
+ * @returns gets ticket or null
  */
-function getLastPendingTicket(userName, repositoryName) {
-    const getTicket = `SELECT * FROM deploys d INNER JOIN deploy_statuses ds
-        WHERE ds.deployUuid = d.uuid
-        AND ds.status = 'PENDING'
+function getPendingTicket(userName, repositoryName) {
+    const getTicket = `SELECT * FROM deploys d WHERE 1=1
+        AND d.status = 'PENDING'
         AND d.engine = 'github'
         AND d.user = '${userName}'
         AND d.repo = '${repositoryName}'
-        ORDER BY ds.date DESC
         LIMIT 1;`
     const list = database.query(getTicket)
     return list[0] || null
