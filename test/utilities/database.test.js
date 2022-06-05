1b9392c870beb14292c2bba9e9756911611c9093
garbagemza <garbagemza@gmail.com>
2022-06-04 21:57:22 -0300

    #4 added coverage

---
+++ test/utilities/database.test.js
@@ -0,0 +1,35 @@
+//
+// database.test (06/04/2022)
+//
+// MIT License
+// Copyright (c) 2022 garbagemza
+
+
+jest.mock('npm-sqlite')
+const database = require('../../utilities/database')
+const sqlite = require('npm-sqlite')
+
+
+test('should init', () => { 
+    const runnerFn = jest.fn().mockImplementation(() => {})
+    sqlite.configure.mockImplementation(runnerFn)
+    database.init()
+    expect(runnerFn).toHaveBeenCalled()
+    expect(runnerFn.mock.calls.length).toBe(1)
+})
+
+test('should run in transaction', () => { 
+    const runnerFn = jest.fn().mockImplementation(() => {})
+    sqlite.runInTransaction.mockImplementation(runnerFn)
+    database.runInTransaction()
+    expect(runnerFn).toHaveBeenCalled()
+    expect(runnerFn.mock.calls.length).toBe(1)
+})
+
+test('should query', () => { 
+    const runnerFn = jest.fn().mockImplementation(() => {})
+    sqlite.query.mockImplementation(runnerFn)
+    database.query()
+    expect(runnerFn).toHaveBeenCalled()
+    expect(runnerFn.mock.calls.length).toBe(1)
+})
