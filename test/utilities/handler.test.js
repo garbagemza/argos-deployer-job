2af3a0620850cfd0a9f60279d0a7ead515c76cf3
garbagemza <garbagemza@gmail.com>
2022-06-04 22:36:04 -0300

    #4 added coverage

---
+++ test/utilities/handler.test.js
@@ -0,0 +1,43 @@
+//
+// handler.test (06/04/2022)
+//
+// MIT License
+// Copyright (c) 2022 garbagemza
+
+const { handler } = require('../../utilities')
+
+test('should handle ok', () => { 
+    const statusFn = jest.fn((code) => {})
+    const sendFn = jest.fn(() => {})
+    const doneFn = jest.fn(() => {})
+
+    const res = {
+        status: statusFn,
+        send: sendFn
+    }
+    const fn = (req, res, logger) => {
+        return undefined
+    }
+    handler(fn)({body: 'hello'}, res, doneFn)
+    expect(statusFn.mock.calls.length).toBe(0)
+    expect(sendFn.mock.calls.length).toBe(0)
+    expect(doneFn.mock.calls.length).toBe(0)
+})
+
+test('should throw error', () => {
+    const statusFn = jest.fn((code) => {})
+    const sendFn = jest.fn(() => {})
+    const doneFn = jest.fn(() => {})
+
+    const res = {
+        status: statusFn,
+        send: sendFn
+    }
+    const fn = (req, res, logger) => {
+        throw {message: 'oops, an error'}
+    }
+    handler(fn)({body: {data: 'some data'}}, res, doneFn)
+    expect(statusFn.mock.calls.length).toBe(1)
+    expect(sendFn.mock.calls.length).toBe(1)
+    expect(doneFn.mock.calls.length).toBe(0)
+})
