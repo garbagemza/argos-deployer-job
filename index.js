3e8f33317003478da6c965482b40da625914630b
garbagemza <garbagemza@gmail.com>
2022-06-26 18:17:48 -0300

    #6 extracted logging to new library

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 require('dotenv').config();
 const express = require('npm-express')
-
-const { envcheck, logger, database } = require('./utilities')
+const winston = require('npm-logger')
+const { envcheck, database } = require('./utilities')
 const { validateTicket, validatePostTicket } = require('./models')
 
 const { getTicketController, postTicketController } = require('./controllers')
 
-const _logger = logger()
+const logger = winston('.')
 
 express({
-    verbose: _logger,
+    verbose: logger,
     beforeInitialization: () => {
         // this checks the existence of environment variables, if they are not declared
         // an exception is thrown.
         envcheck(['PORT', 'WORKDIR'])
-        database.init(_logger)
+        database.init(logger)
     },
     addMiddlewares: (app) => {
         app.builder()
@@ -30,7 +30,7 @@ express({
             .build()
     },
     afterInitialization: (app) => {
-        _logger.info(`argos-deploy.port ${process.env.PORT}`)
+        logger.info(`argos-deploy.port ${process.env.PORT}`)
         app.listen(process.env.PORT)
     }
 })
--- utilities/index.js
+++ utilities/index.js
@@ -1,9 +1,7 @@
 const envcheck = require('./envcheck.js')
-const logger = require('./logger.js')
 const database = require('./database.js')
 
 module.exports = {
     envcheck,
-    logger,
     database
 }
